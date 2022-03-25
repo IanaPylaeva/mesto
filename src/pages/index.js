d71bb6987fa5718cdc904ae453f334ab3ec96918
Iana Pylaeva <94806381+IanaPylaeva@users.noreply.github.com>
2022-03-25 13:36:25 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ import Card from '../components/Card.js';
 import FormValidator from '../components/FormValidator.js';
 import PopupWithForm from '../components/PopupWithForm.js';
 import PopupWithImage from '../components/PopupWithImage.js';
-import Section from '../components/Section.js.';
+import Section from '../components/Section.js';
 import UserInfo from '../components/UserInfo.js';
 
 /* Проект 7 Валидация FormValidator */
